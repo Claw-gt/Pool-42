b910d2a86e904fc3096ffffbe72f0a2579092396
Claw-gt <130146887+Claw-gt@users.noreply.github.com>
2023-08-22 18:19:00 +0200

    Add files via upload

---
+++ git_c04/ex03/ft_atoi.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 14:20:01 by clagarci          #+#    #+#             */
+/*   Updated: 2023/08/22 17:04:06 by clagarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int		num;
+	char	c;
+	int		n;
+	int		count;
+	int		i;
+	int		dec;
+
+	i = 0;
+	count = 0;
+	num = 0;
+	dec = 10;
+
+	while (str[i] != '\0') 
+	{
+		if (str[0] == 32 || (str[0] > 8 && str[0] < 14))
+		{	
+			while (str[i] == 32 || (str[i] > 8 && str[i] < 14))
+			{
+				i++;
+			}
+		}	
+		if ((str[i] == 45 || str[i] == 43) && num == 0)
+		{	
+			if (str[i] == 45)
+			{
+				count++;
+			}
+			printf("%d: %c", i, str[i]);
+			i++;
+			if (str[2] == 45)
+				printf("minus");	
+		}
+		if (str[i] <= 57 && str[i] >= 48)
+		{	
+			n = 0;
+			c = '0';
+			while (c != str[i])
+			{	
+				c++;
+				n++;
+			}
+			num = num * dec + n;
+			i++;
+		}
+		else
+			break ;
+	}
+	if (count % 2 == 1)
+		num *= -1;
+	return (num);
+}
