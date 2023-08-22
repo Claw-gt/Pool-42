fa0b62c012db36aae62015e05ee3ef77bf5fd5eb
Claw-gt <130146887+Claw-gt@users.noreply.github.com>
2023-08-22 12:34:03 +0200

    Add files via upload

--- git_c03/ex05/main.c
+++ git_c03/ex05/main.c
@@ -6,7 +6,7 @@
 /*   By: clagarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 10:48:14 by clagarci          #+#    #+#             */
-/*   Updated: 2023/08/22 12:19:48 by clagarci         ###   ########.fr       */
+/*   Updated: 2023/08/22 12:31:48 by clagarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 int	main(void)
 {
 	char dest[50] = "hola";
-	char src[50] = "ad";
+	char src[50] = "";
 	strlcat(dest, src, 6);
-	//ft_strlcat(dest, src, 6);
+//	ft_strlcat(dest, src, 6);
 	printf("%s", dest);
-//	printf("%lu", strlcat(dest, src, 15));
-	//printf("%u", ft_strlcat(dest, src, 15));
+	printf("%lu", strlcat(dest, src, 15));
+//	printf("%u", ft_strlcat(dest, src, 15));
 	return (0);
 }
