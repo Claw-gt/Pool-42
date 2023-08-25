463db05e78f474134a9d50f9a03e519923f1d728
Claw-gt <130146887+Claw-gt@users.noreply.github.com>
2023-08-25 09:16:37 +0200

    Update ft_strcmp.c

--- git_c03_v2/ex00/ft_strcmp.c
+++ git_c03_v2/ex00/ft_strcmp.c
@@ -24,13 +24,15 @@ int	ft_strcmp(char *s1, char *s2)
 		else
 		{
 			difference = s1[i] - s2[i];
+			return (difference);
 			break ;
 		}
 	}
-	if (s1[i] == '\0' && s2 == '\0')
+	/*if (s1[i] == '\0' && s2 == '\0')
 		difference = 0;
 	else if ((s1[i] != '\0' && s2[i] == '\0')
 		|| (s1[i] == '\0' && s2[i] != '\0'))
-		difference = s1[i] - s2[i];
-	return (difference);
+		difference = s1[i] - s2[i];*/
+	//return (difference);
+	return (0);
 }
